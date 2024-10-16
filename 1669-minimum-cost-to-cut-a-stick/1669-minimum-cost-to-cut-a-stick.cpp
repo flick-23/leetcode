fd4c3f3692f5fddf65735ff2ee55e51c0caab73c
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2024-10-17 00:16:53 +0530

    Time: 15 ms (96.48%), Space: 12.6 MB (18.88%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
+        //stick boundaries
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
-        int m = cuts.size();
+
+        int m = cuts.size();    //total cuts including boundaries
+
+        //DP[i][j] => Minimal cost to cut the stick between cuts[i] & cuts[j]
         vector<vector<int>>dp(m,vector<int>(m,0));
+        //fill dp for increasing lengths of substick
         for(int len = 2; len < m; len++){
+            //i is starting point of the segment
             for(int i = 0; i < m - len; i++){
+                //j is ending point of segment
                 int j = i + len;
                 dp[i][j] = INT_MAX;
+                //try every cut k b/w cuts[i] & cuts[j]
+                //this will help to split the stick optimally
                 for(int k = i + 1;  k < j; k++){
+                    //cost to cut at cuts[k] = len of curr stick segment + cost of cutting left & right segments
                     int cost = cuts[j] - cuts[i] + dp[i][k] + dp[k][j];
+                    //take the min cost of this segment
                     dp[i][j] = min(cost,dp[i][j]);
                 }
             }
         }
+        //the final result is stored in here, which shows min cost to cut stick from 0 to n
         return dp[0][m-1];
     }
 };
