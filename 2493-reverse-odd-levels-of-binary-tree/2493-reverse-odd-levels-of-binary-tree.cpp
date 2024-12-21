adc4255599ef0265ed97341ec43989d5b609d8f8
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2024-12-21 16:36:18 +0530

    Time: 0 ms (100%), Space: 77 MB (75.03%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -12,49 +12,18 @@
  */
 class Solution {
 public:
+    void reverseLevels(TreeNode* left, TreeNode*right, int level){
+        if(!left || !right){
+            return;
+        }
+        if(level % 2 == 0){
+            swap(left->val, right->val);
+        }
+        reverseLevels(left->left, right->right, level+1);
+        reverseLevels(left->right, right->left, level+1);
+    }
     TreeNode* reverseOddLevels(TreeNode* root) {
-        vector<vector<int>> nums(3);
-        vector<int> arr;
-        queue<pair<int, TreeNode*>> q;
-        q.push({0, root});
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int level = it.first;
-            TreeNode* node = it.second;
-            if (nums.size() < 2 * level && level != 0) {
-                nums.resize(2 * level);
-            }
-            nums[level].push_back(node->val);
-            if (node->left)
-                q.push({level + 1, node->left});
-            if (node->right)
-                q.push({level + 1, node->right});
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            if (i % 2 != 0) {
-                reverse(nums[i].begin(),nums[i].end());
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums[i].size(); j++) {
-                arr.push_back(nums[i][j]);
-            }
-        }
-        q.push({0, root});
-        int i = 0;
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int level = it.first;
-            TreeNode* node = it.second;
-            node->val = arr[i++];
-            if (node->left)
-                q.push({level + 1, node->left});
-            if (node->right)
-                q.push({level + 1, node->right});
-        }
-
+        reverseLevels(root->left, root->right, 0);
         return root;
     }
 };
