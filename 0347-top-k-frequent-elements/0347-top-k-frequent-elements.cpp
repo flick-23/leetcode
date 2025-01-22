b6d898a897fe31a8eaebdb31dfa8f9df4a7b2016
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2025-01-22 23:50:59 +0530

    Time: 4 ms (39.64%), Space: 19.9 MB (6.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,28 @@
+/*
+This solution uses a hash map to count the frequency of each element in O(n), followed by a bucket sort approach to organize elements by their frequencies. The bucket array is indexed by frequency, where each bucket contains the elements that appear that many times in the input array. By iterating over the bucket array in reverse (from highest frequency to lowest), we efficiently collect the top k frequent elements in O(n) without needing to sort. This approach reduces the time complexity from O(n \log n) (using a priority queue) to O(n).
+*/
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> m;
-        priority_queue<pair<int, int>> pq;
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) == m.end()) {
-                m[nums[i]] = 1;
-            } else {
-                m[nums[i]]++;
+        vector<int>ans;
+        //num : freq
+        unordered_map<int,int>mpp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            mpp[nums[i]]++;
+        }
+        //add the nums based on the freq, where freq is the index
+        vector<vector<int>>bucket(n+1);
+        for(auto&[num, freq] : mpp){
+            bucket[freq].push_back(num);
+        }
+        for(int i = bucket.size()-1; i >= 0 && ans.size() < k; i--){
+            for(auto num : bucket[i]){
+                ans.push_back(num);
+                if(ans.size() == k){
+                    break;
+                }
             }
-        }
-        for (auto i = m.begin(); i != m.end(); i++) {
-            pq.push(make_pair(i->second, i->first));
-        }
-        while (k--) {
-            ans.push_back(pq.top().second);
-            pq.pop();
         }
         return ans;
     }
