d12b73bf7b416f0b62d8bee7d2e2eb0c6945a9a3
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2025-02-24 18:48:05 +0530

    Time: 21 ms (9.19%), Space: 21 MB (98.84%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,8 +3,9 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int>left(n,0);
+        vector<int>right(n,0);
         left[0]=1;
-        left[n-1]=1;
+        right[n-1]=1;
         for(int i = 1; i < n; i++){
             if(ratings[i] > ratings[i-1]){
                 left[i] = left[i-1] + 1;
@@ -12,16 +13,17 @@ public:
                 left[i] = 1;
             }
         }
-        int candies = 0;
         for(int i = n-2; i >=0; i--){
             if(ratings[i] > ratings[i+1]){
-                left[i] = max(left[i], left[i+1] + 1);
+                right[i] = right[i+1] + 1;
             } else {
-                left[i] = max(left[i],1);
+                right[i] = 1;
             }
-            candies += left[i];
         }
-        candies+=left[n-1];
+        int candies = 0;
+        for(int i = 0; i < n; i++){
+            candies += max(left[i],right[i]);
+        }
         return candies;
     }
 };
