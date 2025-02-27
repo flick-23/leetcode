f4d991509c3ddaa8e05a80f34e38dbf5f74a3e75
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2025-02-27 16:41:17 +0530

    Time: 0 ms (100%), Space: 20.5 MB (61.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int jump, vector<int>&nums, vector<vector<int>>& dp){
-        if(i>= nums.size()-1) return jump;
-        if(dp[i][jump] != -1) return dp[i][jump];
-        int mini = INT_MAX;
-        for(int j = 1; j <= nums[i]; j++){
-            mini = min(mini,solve(i+j,jump+1,nums,dp));
-        }
-        return dp[i][jump] = mini;
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int left = 0, right = 0, maxi = 0, jumps = 0;
-        while(right < n-1){
-            maxi = 0;
-            for(int i = left; i <= right; i++){
-                maxi = max(maxi, nums[i]+i);
+        if (n == 1)
+            return 0;
+        int currentEnd = 0, farthest = 0, jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, nums[i] + i);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+                if (farthest >= n - 1) {
+                    break;
+                }
             }
-            left = right+1;
-            jumps++;
-            right = maxi;
         }
         return jumps;
     }
