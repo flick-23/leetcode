0f66daea7c917b25c61562ef2896a6fd787678cf
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2025-02-27 21:38:32 +0530

    Time: 3 ms (37.85%), Space: 112.5 MB (14.98%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -6,12 +6,13 @@ public:
         for(int i = 0; i < n; i++){
             fullTank += gas[i] - cost[i];
             tank += gas[i] - cost[i];
-
+            // If fuel is negative at any station, reset start position
             if(tank < 0){
-                start = i+1;
-                tank = 0;
-            }
+                start = i+1;    // Restart from next station
+                tank = 0;       // Reset current tank
+            }   
         }
+        // If total fuel is positive, return start index
         return fullTank >=0 ? start : -1;
        
     }
