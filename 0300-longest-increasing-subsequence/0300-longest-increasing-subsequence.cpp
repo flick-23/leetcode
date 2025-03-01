e985b26fe185f44a9a01d98c7b3cdb788d6a0903
Venkatesh Dhongadi <51364640+flick-23@users.noreply.github.com>
2025-03-01 13:11:27 +0530

    Time: 58 ms (64.2%), Space: 14.4 MB (61.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& prices, vector<vector<int>>& dp){
-        if(i == prices.size()) return 0;
-
-        if(dp[i][j+1] != -1)    return dp[i][j+1];
-
-        int notPick = 0 + f(i+1,j,prices,dp);
-        
-        int pick = 0;
-        if(j == -1 || prices[i]>prices[j]){
-            pick = 1 + f(i+1,i,prices,dp);
-        }
-        return dp[i][j+1] = max(pick, notPick);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return f(0,-1,nums,dp);
+        int maxLen = 1;
+        vector<int>dp(n,1); // Initialize LIS length for each index as 1
+        for(int i = 1; i < n; i++){      // Traverse all elements
+            for(int j = 0; j < i; j++){     // Check all previous elements
+                if(nums[j] < nums[i])       // Can nums[i] extend LIS ending at j?
+                    dp[i] = max(dp[i], dp[j]+1);
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+        return maxLen;
     }
 };
